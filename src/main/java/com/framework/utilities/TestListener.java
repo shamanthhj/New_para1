9fa1183544f7609f0a6ec98f4c47f06ddc6416cf
shamanthhj <shamanthhj6@gmail.com>
2026-01-08 22:42:11 +0530

    Update TestListener.java

--- src/main/java/com/framework/utilities/TestListener.java
+++ src/main/java/com/framework/utilities/TestListener.java
@@ -1,11 +1,14 @@
 package com.framework.utilities;
 
-
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import com.framework.base.BaseClass;
 
 public class TestListener extends BaseClass implements ITestListener{
