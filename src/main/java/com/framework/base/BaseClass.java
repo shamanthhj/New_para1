2c48dc8b43efd40e1778cb25140bcb902e40d74e
shamanthhj <shamanthhj6@gmail.com>
2026-01-18 12:19:33 +0530

    Update BaseClass.java

--- src/main/java/com/framework/base/BaseClass.java
+++ src/main/java/com/framework/base/BaseClass.java
@@ -19,52 +19,62 @@ import com.framework.utilities.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
-
-    public static WebDriver driver;
-    public static WebDriverWait wait;
-
-    @BeforeMethod
-    public void setup() {
-
-        String browser = ConfigReader.getProperty("browser");
-        String url = ConfigReader.getProperty("url");
-        int WaitTime = Integer.parseInt(ConfigReader.getProperty("implicitWait"));
-
-        if(browser.equalsIgnoreCase("chrome")) {
-
-            WebDriverManager.chromedriver().setup();
-
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
+	public static WebDriver driver;
+	//public static WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	public static ExtentReports extent;
+	public static ExtentTest test;
+	
+	@BeforeMethod
+	public void setup()
+	{
+		String browser = ConfigReader.getProperty("browser");
+		String url = ConfigReader.getProperty("url");
+		int WaitTime = Integer.parseInt(ConfigReader.getProperty("implicitWait"));
+		
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--remote-allow-origins=*");
             options.addArguments("--window-size=1920,1080");
 
             driver = new ChromeDriver(options);
+		} else if(browser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}else if(browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}else {
+			throw new RuntimeException("Unsupported browser: " + browser);
+		}
 
-        } else if(browser.equalsIgnoreCase("edge")) {
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitTime));
+		driver.get(url);
+				
+		/*if(extent == null)
+		{
+			String path = System.getProperty("user.dir") + "/reports/ExtentReport.html";
+			ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+			reporter.config().setReportName("Automation Test Report");
+			reporter.config().setDocumentTitle("Test Result");
+			
+			extent = new ExtentReports();
+			extent.attachReporter(reporter);
+			extent.setSystemInfo("Tester", "Shyam");
+		}*/
+	}
+	
+	@AfterMethod
+	public void teardown()
+	{
+		if(driver != null) {
+		driver.quit();
+		}
+	}
 
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
-
-        } else if(browser.equalsIgnoreCase("firefox")) {
-
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-
-        } else {
-            throw new RuntimeException("Unsupported browser: " + browser);
-        }
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitTime));
-        driver.get(url);
-    }
-
-    @AfterMethod
-    public void teardown() {
-        if(driver != null) {
-            driver.quit();
-        }
-    }
 }
