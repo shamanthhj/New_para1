5aab168111dda73cbcb5d7daf806d0569596d0fc
shamanthhj <shamanthhj6@gmail.com>
2026-01-28 18:41:06 +0530

    Update TransferFundsTest.java

--- src/test/java/com/framework/tests/TransferFundsTest.java
+++ src/test/java/com/framework/tests/TransferFundsTest.java
@@ -19,7 +19,7 @@ public class TransferFundsTest extends BaseClass{
 		login.loginAs(username, password);
 		
 		TransferFundsPage tfPage = new TransferFundsPage();
-        tfPage.transferFunds("13899", "16563", "500");
+        tfPage.transferFunds("15120", "15120", "500");
 
        // Assert.assertTrue(tfPage.isTransferSuccessful(), "Transfer failed!");
 	}
